b61f50d2177a2f90bc0863654f6cc8f9a28e67d3
Steve Smith <steve.a.smith@avanade.com>
2023-02-22 17:11:59 +0000

    inital commit with docker

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Really Simple Remix App</h1>
+      <p>For the purpose of a quick prototype for CI/CD.</p>
+    </div>
+  );
+}
