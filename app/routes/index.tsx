294cf179a6fe4ecb1fe6af3b050df8ab335b3139
Steve Smith <steve.a.smith@avanade.com>
2023-03-07 17:04:48 +0000

    small change

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>A Really Really Simple Remix App</h1>
+      <h1>A Really Simple Remix App</h1>
       <strong>For the purpose of a quick prototype for CI/CD.</strong>
     </div>
   );
